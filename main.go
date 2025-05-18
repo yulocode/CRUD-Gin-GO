1b560936ac1fe76493b3c0a88590d866f67abb55
$yulo <101001858+yulocode@users.noreply.github.com>
2025-05-18 16:56:29 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("person", getPersons)
+		v1.GET("person/:id", getPersonById)
+		v1.POST("person", addPerson)
+		v1.PUT("person/:id", updatePerson)
+		v1.DELETE("person/:id", deletePerson)
+		v1.OPTIONS("person", options)
+	}
+}
 
-	router.GET("/home", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
-	})
-	router.Run(":8080")
+func getPersons(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "getPersons Called"})
+}
+
+func getPersonById(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "getPersonsById " + id + " Called"})
+}
+
+func addPerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "addPerson Called"})
+}
+
+func updatePerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "updatePerson Called"})
+}
+
+func deletePerson(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deletePersons " + id + " Called"})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "options Called"})
 }
